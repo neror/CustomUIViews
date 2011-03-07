1ec1d1835d49b94978db9403c64538d384868cda
Nathan Eror <bignate@gmail.com>
2011-03-07 13:26:41 -0600

    Code cleanup and added license headers where there were none.

--- Classes/BitmapFontCounterController.m
+++ Classes/BitmapFontCounterController.m
@@ -1,10 +1,34 @@
+/*
+ The MIT License
+ 
+ Copyright (c) 2011 Free Time Studios and Nathan Eror
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
 #import "BitmapFontCounterController.h"
 #import "CounterView.h"
 
 @implementation BitmapFontCounterController
 
-@synthesize textEntry;
-@synthesize counterView;
+@synthesize textEntry = textEntry_;
+@synthesize counterView = counterView_;
 
 - (void)viewDidLoad {
   self.title = @"Bitmap Font Counter";
@@ -14,8 +38,8 @@
 }
 
 - (void)dealloc {
-  self.textEntry = nil;
-  self.counterView = nil;
+  [textEntry_ release], textEntry_ = nil;
+  [counterView_ release], counterView_ = nil;
   [super dealloc];
 }
 
--- Classes/ProgressBarView.m
+++ Classes/ProgressBarView.m
@@ -1,9 +1,34 @@
+/*
+ The MIT License
+ 
+ Copyright (c) 2011 Free Time Studios and Nathan Eror
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
 #import "ProgressBarView.h"
 
 @interface ProgressBarView() {
-  CAGradientLayer *progressBar_;
   CGFloat progress_;
 }
+
+@property (nonatomic, retain) CAGradientLayer *progressBar;
 
 - (void)setupLayers;
 
@@ -11,10 +36,12 @@
 
 @implementation ProgressBarView
 
+@synthesize progressBar = progressBar_;
+
 #pragma mark - Object Lifecycle / Memory management
 
 - (void)dealloc {
-  [progressBar_ release];
+  [progressBar_ release], progressBar_ = nil;
   [super dealloc];
 }
 
@@ -31,8 +58,8 @@
 
 - (void)setupLayers {
   self.progress = 1.f;
-  progressBar_ = [[CAGradientLayer alloc] init];
-  [self.layer addSublayer:progressBar_];
+  self.progressBar = [CAGradientLayer layer];
+  [self.layer addSublayer:self.progressBar];
 }
 
 #pragma mark - Drawing and Layout
@@ -44,19 +71,19 @@
   self.layer.cornerRadius = 12.f;
   self.layer.masksToBounds = YES;
   
-  progressBar_.colors = [NSArray arrayWithObjects:
-                         (id)[[UIColor blueColor] CGColor], 
-                         (id)[[UIColor colorWithHue:.6f saturation:.8f brightness:1.f alpha:1.f] CGColor],
-                         (id)[[UIColor blueColor] CGColor], nil];
-  progressBar_.locations = [NSArray arrayWithObjects:
-                            [NSNumber numberWithFloat:0.f],
-                            [NSNumber numberWithFloat:.5f],
-                            [NSNumber numberWithFloat:1.f], nil];
-  progressBar_.anchorPoint = CGPointZero;
+  self.progressBar.colors = [NSArray arrayWithObjects:
+                             (id)[[UIColor blueColor] CGColor], 
+                             (id)[[UIColor colorWithHue:.6f saturation:.8f brightness:1.f alpha:1.f] CGColor],
+                             (id)[[UIColor blueColor] CGColor], nil];
+  self.progressBar.locations = [NSArray arrayWithObjects:
+                                [NSNumber numberWithFloat:0.f],
+                                [NSNumber numberWithFloat:.5f],
+                                [NSNumber numberWithFloat:1.f], nil];
+  self.progressBar.anchorPoint = CGPointZero;
   
   CGRect progressBounds = self.bounds;
   progressBounds.size.width *= self.progress;
-  progressBar_.bounds = progressBounds;
+  self.progressBar.bounds = progressBounds;
 }
 
 #pragma mark - Getters and Setters
